32ab6de1bda0c4bc35b2d03134a91af2c4f2d6a6
Frank Bergmann <frank.bergmann@project-open.com>
2016-06-21 08:41:04 +0000

    - Moved upgrade scripts to upgrade-4.1-5.0 package

--- sql/postgresql/upgrade/upgrade-5.0.0.0.0-5.0.0.0.1.sql
+++
@@ -1,5 +0,0 @@
--- upgrade-5.0.0.0.0-5.0.0.0.1.sql
-SELECT acs_log__debug('/packages/intranet-crm-opportunities/sql/postgresql/upgrade/upgrade-5.0.0.0.0-5.0.0.0.1.sql','');
-
--- Create Project makes no sense for closed opportunities 
-update im_view_columns set column_render_tcl = '$project_link' where column_id = 98110; 
