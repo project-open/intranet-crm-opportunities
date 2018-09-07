1f54ef11681891a4e766b6fd8091ad59cf034c87
Frank Bergmann <frank.bergmann@project-open.com>
2018-09-07 15:40:19 +0000

    - made CRM now multi-sales capable with permissions per opportunity

--- sql/postgresql/intranet-crm-opportunities-create.sql
+++ sql/postgresql/intranet-crm-opportunities-create.sql
@@ -236,18 +236,20 @@ SELECT im_component_plugin__new (
 	null,					-- view_name
 	100,					-- sort_order
 	'im_ad_hoc_query -format html -package_key intranet-crm-opportunities "
-		select
-			''<a href=/intranet/companies/view?company_id='' || cust.company_id || ''>'' || cust.company_name || ''</a>'' as company,
+		select	''<a href=/intranet/companies/view?company_id='' || cust.company_id || ''>'' || cust.company_name || ''</a>'' as company,
 			''<a href=/intranet/projects/view?project_id='' || p.project_id || ''>'' || p.project_name || ''</a>'' as opportunity,
-			p.presales_value,
-			p.presales_probability,
-			p.presales_value * p.presales_probability as weighted_value
+			round(p.presales_value) || ''[im_default_currency]'' as presales_value,
+			round(p.presales_probability) || ''%'' as presales_probability,
+			round(p.presales_value * p.presales_probability) as weighted_value
 		from	im_projects p,
 			im_companies cust
 		where	p.parent_id is null and
+			p.company_id = cust.company_id and
 			p.opportunity_sales_stage_id not in (select * from im_sub_categories(84018)) and
-			p.project_type_id in (select * from im_sub_categories(102))
-			p.company_id = cust.company_id
+			p.project_type_id in (select * from im_sub_categories(102)) and
+			(1 = [im_permission [auth::require_login] view_projects_all] OR
+			p.project_id in (select r.object_id_one from acs_rels r where r.object_id_two = [auth::require_login])
+			)
 		order by weighted_value DESC
 		limit 10
 "',   -- component_tcl
