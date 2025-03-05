c14f03673699a77c6e50ecbb6efe7c1befbf549a
Project Open <info@project-open.com>
2025-03-05 14:08:25 +0000

    v52: Cleanup upgrade scripts

--- sql/postgresql/upgrade/upgrade-5.0.4.0.0-5.0.4.0.1.sql
+++
@@ -1,5 +0,0 @@
-SELECT acs_log__debug('/packages/intranet-crm-opportunities/sql/postgresql/upgrade/upgrade-5.0.4.0.0-5.0.4.0.1.sql','');
-
-update im_component_plugins
-set enabled_p = 'f'
-where plugin_name = 'Opportunity Pipeline';
